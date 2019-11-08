1c077974d453794244e44b8a42905b1e260cba69
Androz2091 <androz2091@gmail.com>
2019-11-08 19:14:08 +0100

    :mute: Remove dashboard logs

--- dashboard/auth/CheckAuth.js
+++ dashboard/auth/CheckAuth.js
@@ -1,9 +1,7 @@
 module.exports = async (req, res, next) => {
     if(req.user){
-        console.log("User found");
 	    return next();
     } else {
-        console.log("User not found");
         let redirectURL = ((req.originalUrl.includes("login") || req.originalUrl === "/") ? "/selector" : req.originalUrl);
         let state = req.client.functions.randomID();
         req.client.states[state] = redirectURL;
