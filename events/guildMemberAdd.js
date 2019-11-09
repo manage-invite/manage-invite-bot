16eda6c13b32a6b328c1c1e9e6a0bd082e64698a
Androz2091 <androz2091@gmail.com>
2019-11-09 10:35:52 +0100

    :bug: Use joinDM instead of join message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -132,7 +132,7 @@ module.exports = class {
 
         // DM Join messages
         if(guildData.joinDM.enabled && guildData.joinDM.message && invite && guildData.premium){
-            let formattedMessage = this.client.functions.formatMessage(guildData.join.message, member, inviter, invite, (guildData.language || "english").substr(0, 2), inviterData);
+            let formattedMessage = this.client.functions.formatMessage(guildData.joinDM.message, member, inviter, invite, (guildData.language || "english").substr(0, 2), inviterData);
             member.send(formattedMessage);
         }
 
