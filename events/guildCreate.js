a5a0be58712beaa3ab6afc73c3147c058d9cceb5
Androz2091 <androz2091@gmail.com>
2019-11-10 12:49:46 +0100

    :bug: Wait 5 seconds before synchronize invites

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -19,19 +19,9 @@ module.exports = class {
         }
 
         let guildData = await this.client.guildsData.findOne({ id: guild.id });
-        let welcomeMessage = "My prefix is `+`. If you want to remove server invites to start over from scratch, you can use `+remove-invites`.\n \n**--------------**\n";
-        if(!guildData){
-            guildData = await this.client.findOrCreateGuild({ id: guild.id });
-            let guildInvites = await message.guild.fetchInvites();
-            let users = new Set(guildInvites.map((i) => i.inviter.id));
-            await this.client.functions.asyncForEach(Array.from(users), async (user) => {
-                let memberData = await this.client.findOrCreateGuildMember({ id: user, guildID: guild.id });
-                memberData.invites = guildInvites.filter((i) => i.inviter.id === user).map((i) => i.uses).reduce((p, c) => p + c);
-                await memberData.save();
-            });
-        } else {
-            welcomeMessage = `My prefix is \`${guildData.prefix}\`. If you want to remove server invites to start over from scratch, you can use \`${guildData.prefix}remove-invites\`. If you want to synchronize current server invites with the bot, you can use \`${guildData.prefix}sync-invites\`\n \n**--------------**\n`
-        }
+        let welcomeMessage = guildData ?
+        `My prefix is \`${guildData.prefix}\`. If you want to remove server invites to start over from scratch, you can use \`${guildData.prefix}remove-invites\`. If you want to synchronize current server invites with the bot, you can use \`${guildData.prefix}sync-invites\`\n \n**--------------**\n`
+        : "My prefix is `+`. If you want to remove server invites to start over from scratch, you can use `+remove-invites`.\n \n**--------------**\n";            
 
         const guildCreate = JSON.stringify(new Discord.MessageEmbed()
         .setTitle("Add | :heart:")
@@ -51,13 +41,24 @@ module.exports = class {
         let joinEmbed = new Discord.MessageEmbed()
         .setTitle("Add | :heart:")
         .setDescription(`Hello ${inviter.username}! Thanks for adding me to your server !\n\n **--------------** `)
-        .addField("__**INFORMATIONS**__", )
-        .addField("__**HELP**__", "If you need some help join the support server !\n \n**--------------**\n")
+        .addField("__**INFORMATIONS**__", welcomeMessage)
+        .addField("__**HELP**__", "If you need some help join the support server!\n \n**--------------**\n")
         .addField("__**LINKS**__", `> Add the bot [[Click here]](https://discordapp.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=2146958847&scope=bot)\n> Support server  [[Click here]](${this.client.config.discord})\n> Dashboard  [[Click here]](${this.client.config.baseURL}) `)
         .setFooter(this.client.config.footer)
         .setTimestamp()
         .setColor(this.client.config.color)
     
         inviter.send(joinEmbed);
+
+        await this.client.wait(5000);
+        let client = this.client;
+        let guildInvites = await guild.fetchInvites();
+        if(!guildInvites) return;
+        let users = new Set(guildInvites.map((i) => i.inviter.id));
+        await this.client.functions.asyncForEach(Array.from(users), async (user) => {
+            let memberData = await client.findOrCreateGuildMember({ id: user, guildID: guild.id });
+            memberData.invites = guildInvites.filter((i) => i.inviter.id === user).map((i) => i.uses).reduce((p, c) => p + c);
+            await memberData.save();
+        });
     }
 };
