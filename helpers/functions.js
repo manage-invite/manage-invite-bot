29c4e7b6777a1613c22166e751b28a89a12d0a1b
Androz2091 <androz2091@gmail.com>
2020-01-12 11:32:08 +0100

    :sparkles: Add isEqual() helper function

--- helpers/functions.js
+++ helpers/functions.js
@@ -183,6 +183,47 @@ const joinedXDays = (numberOfDays, members) => {
     return days.reverse();
 };
 
+/**
+ * Compare two arrays
+ * @param {Array} value The first array
+ * @param {Array} other The second array
+ * @returns {Boolean} Whether the arrays are equals
+ */
+const isEqual = (value, other) => {
+	let type = Object.prototype.toString.call(value);
+	if (type !== Object.prototype.toString.call(other)) return false;
+	if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+	let valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+	let otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+	if (valueLen !== otherLen) return false;
+	const compare = (item1, item2) => {
+		let itemType = Object.prototype.toString.call(item1);
+		if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+			if (!isEqual(item1, item2)) return false;
+		}
+		else {
+			if (itemType !== Object.prototype.toString.call(item2)) return false;
+			if (itemType === '[object Function]') {
+				if (item1.toString() !== item2.toString()) return false;
+			} else {
+				if (item1 !== item2) return false;
+			}
+		}
+	};
+	if (type === '[object Array]') {
+		for (var i = 0; i < valueLen; i++) {
+			if (compare(value[i], other[i]) === false) return false;
+		}
+	} else {
+		for (var key in value) {
+			if (value.hasOwnProperty(key)) {
+				if (compare(value[key], other[key]) === false) return false;
+			}
+		}
+	}
+	return true;
+};
+
 module.exports = {
     asyncForEach,
     formatMessage,
@@ -191,5 +232,6 @@ module.exports = {
     assignRanks,
     postTopStats,
     lastXDays,
-    joinedXDays
+    joinedXDays,
+    isEqual
 };
