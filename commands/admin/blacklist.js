a2a5a0451018b38f4f550a355a0c8d03259291a7
Androz2091 <androz2091@gmail.com>
2020-02-20 11:01:33 +0100

    :sparkles: add blacklist command

---
+++ commands/admin/blacklist.js
@@ -0,0 +1,60 @@
+const Command = require("../../structures/Command.js"),
+Discord = require("discord.js");
+
+class Blacklist extends Command {
+    constructor (client) {
+        super(client, {
+            name: "blacklist",
+            enabled: true,
+            aliases: [],
+            clientPermissions: [ "EMBED_LINKS" ],
+            permLevel: 2
+        });
+    }
+
+    async run (message, args, data) {
+        const embed = new Discord.MessageEmbed()
+        .setColor(data.color)
+        .setFooter(data.footer);
+        const action = args[0];
+        switch(action){
+            case "add": {
+                const user = message.mentions.users.first() || await this.client.users.fetch(args[0]).catch(() => {});
+                if(!user) return message.channel.send(message.language.blacklist.mentions.add());
+                data.guild.blacklistedUsers.push(user.id);
+                await data.guild.save();
+                message.channel.send(message.language.blacklist.success.add(user));
+                break;
+            };
+            case "remove": {
+                const user = message.mentions.users.first() || await this.client.users.fetch(args[0]).catch(() => {});
+                if(!user) return message.channel.send(message.language.blacklist.mentions.remove());
+                if(!data.guild.blacklistedUsers.includes(user.id)) return message.channel.send(message.language.blacklist.notFound(user));
+                data.guild.blacklistedUsers = data.guild.blacklistedUsers.filter((u) => u !== user.id);
+                await data.guild.save();
+                message.channel.send(message.language.blacklist.success.remove(user));
+                break;
+            }
+            case "list": {
+                if(data.guild.blacklistedUsers.length < 1){
+                    embed.setDescription(message.language.blacklist.empty());
+                } else {
+                    let users = [];
+                    await this.client.functions.asyncForEach(data.guild.blacklistedUsers, async (userID) => {
+                        const user = await this.client.users.fetch(userID);
+                        users.push(`${user.tag} (${user.toString()})`);
+                    });
+                    embed.setDescription(users.join("\n"));
+                }
+                message.channel.send(embed);
+                break;
+            }
+            default: {
+                message.channel.send(message.language.blacklist.action.error());
+            }
+        }
+    }
+
+};
+
+module.exports = Blacklist;
