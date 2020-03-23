5822019daa55f07175febf2571146cbf8d261920
Androz2091 <androz2091@gmail.com>
2020-03-23 10:59:16 +0100

    :sparkles: Add guildData to guild#data

--- events/message.js
+++ events/message.js
@@ -15,8 +15,7 @@ module.exports = class {
 
         if(!message.guild || message.author.bot) return;
 
-        const guildData = await this.client.database.fetchGuild(message.guild.id);
-        data.guild = guildData;
+        const guildData = data.guild = message.guild.data = await this.client.database.fetchGuild(message.guild.id);
         message.language = require("../languages/"+data.guild.language);
     
         if(message.content.match(new RegExp(`^<@!?${this.client.user.id}>( |)$`))) return message.reply(message.language.utils.prefix(data.guild.prefix));
