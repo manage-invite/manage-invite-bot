6acd1e3ea79ba5eea7df25506e05151ac6b5b364
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-26 17:57:41 +0400

    Update addbonus.js

--- commands/admin/addbonus.js
+++ commands/admin/addbonus.js
@@ -16,7 +16,7 @@ class AddBonus extends Command {
 
         const bonus = args[0];
         if(!bonus) return message.channel.send(message.language.addbonus.errors.bonus.missing(data.guild.prefix));
-        if(isNaN(bonus) || (parseInt(bonus) < 1) || !Number.isInteger(parseInt(bonus))) return message.channel.send(message.language.addbonus.errors.bonus.incorrect(data.guild.prefix));
+        if(Number.isNaN(bonus) || (parseInt(bonus) < 1) || (parseInt(bonus) >= 9223372036854775800) || !Number.isInteger(parseInt(bonus))) return message.channel.send(message.language.addbonus.errors.bonus.incorrect(data.guild.prefix));
 
         const member = message.mentions.members.first() || await this.client.resolveMember(args.slice(1).join(" "), message.guild);
         if(!member) return message.channel.send(message.language.addbonus.errors.member.missing(data.guild.prefix));
