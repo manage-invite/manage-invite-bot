35b1f3935463c640b594618729691eef04772477
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-06-13 14:20:14 +0200

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -2,6 +2,8 @@ const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./index.js", {
     token: require("./config").token,
     totalShards: require("./config").shardCount,
+    autoSpawn: true,
+    respawn: true,
     shardArgs: [ ...process.argv, ...[ '--sharded' ] ]
 });
 
