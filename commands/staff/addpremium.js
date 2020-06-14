c058e2cfa9fc4bfcfa4f9a89fbf68d3a8e30d885
Androz2091 <androz2091@gmail.com>
2020-06-14 11:59:59 +0200

    :sparkles: Add addpremium command

--- commands/staff/addpremium.js
+++ commands/staff/addpremium.js
@@ -7,11 +7,50 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "add-premium" ],
             clientPermissions: [],
-            permLevel: 4
+            permLevel: 5
         });
     }
 
     async run (message, args, data) {
+
+        const premiumArgs = {
+            guildID: args[0],
+            daysCount: parseInt(args[1]),
+            amount: parseInt(args[2]),
+            user: message.mentions.users.first() || await this.client.users.fetch(user).catch(() => {}),
+            pmtType: args[3],
+            guildsCount: parseInt(args[4]),
+            label: parseInt(args[5])
+        }
+
+        Object.keys(premiumArgs).forEach((key) => {
+            if(!premiumArgs[key]){
+                return message.channel.send(`${this.client.emojis.error} | Invalid args. ${Object.keys(premiumArgs).join(', ')}. Missing **${key}**.`);
+            }
+        });
+
+        const createdAt = new Date();
+
+        const paymentID = await this.client.database.createPayment({
+            payerDiscordID: premiumArgs.user.id,
+            payerDiscordUsername: premiumArgs.user.tag,
+            modID: message.author.id,
+            amount: premiumArgs.amount,
+            type: premiumArgs.pmtType,
+            createdAt
+        });
+
+        const subscription = await this.client.database.createSubscription({
+            expiresAt: new Date(Date.now()+(premiumArgs.daysCount*24*60*60*1000)),
+            createdAt,
+            guildsCount: premiumArgs.guildsCount,
+            subLabel: premiumArgs.label
+        }, false);
+        await this.client.database.createSubPaymentLink(subscription.id, paymentID);
+        await this.client.database.createGuildSubLink(premiumArgs.guildID, subscription.id);
+        await subscription.fetchGuilds();
+
+        return message.channel.send(`${this.client.emojis.success} | Subscription created. Get more informations with \`${message.guild.data.prefix}sub ${premiumArgs.guildID}\`.`);
 
     }
 };
