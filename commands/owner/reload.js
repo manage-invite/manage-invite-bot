86d378bcac7620fd91068b0dd3d15fa153044c2f
Androz2091 <androz2091@gmail.com>
2020-06-17 20:34:01 +0200

    :bug: Update reload command with shards

--- commands/owner/reload.js
+++ commands/owner/reload.js
@@ -15,16 +15,26 @@ module.exports = class extends Command {
     async run (message, args, data) {
         const command = args[0];
         if(command === "l"){
-            const i18n = require("../../helpers/i18n");
-            this.client.translations = await i18n();
+            const path = require("path");
+            const file = path.join(__dirname, '..', '..', 'helpers', 'i18n.js');
+            this.client.shard.broadcastEval(`
+                const i18n = require('${file}');
+                i18n().then((r) => {
+                    this.translations = r;
+                });
+            `);
             return message.channel.send(this.client.config.emojis.success+" | Languages reloaded!");
         }
         const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
         if(!cmd){
             message.channel.send(this.client.config.emojis.error+" | Cannot find command `"+command+"`!");
         }
-        await this.client.unloadCommand(cmd.conf.location, cmd.help.name);
-        await this.client.loadCommand(cmd.conf.location, cmd.help.name);
+        await this.client.shard.broadcastEval(`
+            delete require.cache['${cmd.conf.location}'];
+            this.unloadCommand('${cmd.conf.location}', '${cmd.help.name}').then(() => {
+                this.loadCommand('${cmd.conf.location}', '${cmd.help.name}');
+            });
+        `);
         message.channel.send(this.client.config.emojis.success+" | `"+command+"` reloaded!");
     }
 
