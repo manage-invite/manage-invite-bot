9454c92d2b1d845474cd2be4730b238c7225c18f
Androz2091 <androz2091@gmail.com>
2020-08-21 10:14:03 +0200

    :bug: Mark client as fetched every quarter

--- events/ready.js
+++ events/ready.js
@@ -74,7 +74,20 @@ module.exports = class {
                 this.client.database.saveStats(totalGuildsCreated, totalGuildsDeleted, totalCommandsRan, totalPgQueries, new Date());
             }, null, true, "America/Los_Angeles");
         }
-        
+
+        new CronJob("0 */15 * * * *", async () => {
+            if(this.client.fetched){
+                const guildsToFetch = this.client.guilds.cache.filter((guild) => !this.client.invitations[guild.id]).array();
+                this.client.logger.log(`${guildsToFetch.length} guilds need to be fetched`);
+                await this.client.functions.asyncForEach(guildsToFetch, async (guild) => {
+                    const member = await guild.members.fetch(this.client.user.id).catch(() => {});
+                    const i = process.argv.includes("--uncache") ? new Map() : (member.hasPermission("MANAGE_GUILD") ? await guild.fetchInvites().catch(() => {}) : new Map());
+                    this.client.invitations[guild.id] = i || new Map();
+                });
+                this.client.fetched = true;
+            }
+        }, null, true, "America/Los_Angeles");
+
         this.client.on("shardReady", (shardID) => {
             this.client.shard.broadcastEval(`
                 let logsChannel = this.channels.cache.get(this.config.shardLogs);
@@ -103,18 +116,6 @@ module.exports = class {
                 if(logsChannel) logsChannel.send(emojis.online+' | Shard #${shardID} has resumed!');
             `);
         });
-
-        new CronJob("0 */15 * * * *", async () => {
-            if(this.client.fetched){
-                const guildsToFetch = this.client.guilds.cache.filter((guild) => !this.client.invitations[guild.id]).array();
-                this.client.logger.log(`${guildsToFetch.length} guilds need to be fetched`);
-                await this.client.functions.asyncForEach(guildsToFetch, async (guild) => {
-                    const member = await guild.members.fetch(this.client.user.id).catch(() => {});
-                    const i = process.argv.includes("--uncache") ? new Map() : (member.hasPermission("MANAGE_GUILD") ? await guild.fetchInvites().catch(() => {}) : new Map());
-                    this.client.invitations[guild.id] = i || new Map();
-                });
-            }
-        }, null, true, "America/Los_Angeles");
 
     }
 };
