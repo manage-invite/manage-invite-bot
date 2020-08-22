761d2d4021f8ebf357ed226b4a2151c5491bd0c6
Androz2091 <androz2091@gmail.com>
2020-08-22 08:14:40 +0200

    :sparkles: Dynamic permissions check

--- helpers/permissions.js
+++ helpers/permissions.js
@@ -22,7 +22,19 @@ module.exports = [
     {
         level: 4,
         name: "Bot moderator",
-        check: (message) => [ "654754795336237058", "456500252048883714", "547514927019982864", "246993557381054465", "675636857463373847", "592782178350399673" ].includes(message.author.id)    },
+        check: (message) => {
+            return (
+                [ "654754795336237058", "456500252048883714", "547514927019982864", "246993557381054465", "675636857463373847", "592782178350399673" ].includes(message.author.id)
+                || (
+                    message.client.guilds.cache.has(message.client.config.supportServer)
+                        ? (
+                            message.client.guilds.cache.get(message.client.config.supportServer).members.cache.get(message.author.id)
+                                ? message.client.guilds.cache.get(message.client.config.supportServer).members.cache.get(message.author.id).roles.cache.has(message.client.config.modRole)
+                                : false)
+                        : false)
+            );
+        }
+    },
     {
         level: 5,
         name: "Bot owner",
