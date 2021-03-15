83c8a9769177dcbe124ed660ddec8775ed933440
WUMPUS9999 <77828524+WUMPUS9999@users.noreply.github.com>
2021-03-15 17:40:21 +0545

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -24,56 +24,24 @@ module.exports = {
     /* Logs */
     addLogs:        "XXXXXXXXXXX",
     removeLogs:     "XXXXXXXXXXX",
-    dashLogs:       "XXXXXXXXXXX",
     statsLogs:      "XXXXXXXXXXX",
-    premiumLogs:    "XXXXXXXXXXX",
     voteLogs:       "XXXXXXXXXXX",
-    /* Dashboard */
-    secret:         "XXXXXXXXXXX",
-    baseURL:        "XXXXXXXXXXX",
-    port:           3100,
-    pswd:           "XXXXXXXXXXX",
-    failureURL:     "https://docs.manage-invite.xyz",
+
     /* Other */
     mongodb:        "XXXXXXXXXXX",
     discord:        "XXXXXXXXXXX",
     prefix:         "+",
     owners: [ "XXXXXXXXXXX", "XXXXXXXXXXX" ],
-    modRole:        "XXXXXXXXXXX",
-    premiumRole:    "XXXXXXXXXXX",
+    modRole:        "XXXXXXXXXXX"
     supportServer:  "XXXXXXXXXXX",
     sentryDSN:      "XXXXXXXXXXX",
-    /* Top.gg */
-    topToken: "XXXXXXXXXXX",
-    topAuth: "XXXXXXXXXXX",
+
     /* Database */
     database: {
-        user: "",
+        user: "user1",
         host: "localhost",
         database: "manage_invite",
-        password: "",
+        password: "1234S978",
         port: 5432
     },
-    /* PayPal */
-    paypal: {
-        mode: "sandbox",
-        live: {
-            email: "XXXXXXXXXXX",
-            returnURL: "https://dash.manage-invite.xyz/payment/callback",
-            cancelURL: "https://dash.manage-invite.xyz/selector",
-            ipnURL: "https://dash.manage-invite.xyz/payment/ipn",
-            formURL: "https://www.paypal.com/cgi-bin/webscr",
-            fetchURL: "https://ipnpb.paypal.com/cgi-bin/webscr?cmd=_notify-validate",
-            ptdToken: "XXXXXXXXXXX"
-        },
-        sandbox: {
-            email: "XXXXXXXXXXX",
-            returnURL: "http://localhost:3100/payment/callback",
-            cancelURL: "http://localhost:3100/",
-            ipnURL: "http://ngork.io/payment/ipn",
-            formURL: "https://www.sandbox.paypal.com/cgi-bin/webscr",
-            fetchURL: "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr?cmd=_notify-validate",
-            pdtToken: "XXXXXXXXXXX"
-        }
-    }
-};
+
