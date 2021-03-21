292b0e2ae71fecb9f7f820b47ef10c3849037c66
Androz2091 <androz2091@gmail.com>
2021-03-21 14:49:14 +0100

    :zap: Update migration script

--- scripts/migration-47-48.js
+++ scripts/migration-47-48.js
@@ -25,29 +25,6 @@ const tasks = [
         }
     },
     {
-        name: "Create random_string function",
-        execute: () => {
-            return pool.query(`
-                Create or replace function random_string(length integer) returns text as
-                $$
-                declare
-                chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
-                result text := '';
-                i integer := 0;
-                begin
-                if length < 0 then
-                    raise exception 'Given length cannot be less than 0';
-                end if;
-                for i in 1..length loop
-                    result := result || chars[1+random()*(array_length(chars, 1)-1)];
-                end loop;
-                return result;
-                end;
-                $$ language plpgsql;
-            `);
-        }
-    },
-    {
         name: "Add the guild_storage_id column to the guilds table",
         execute: () => {
             return pool.query("ALTER TABLE guilds ADD COLUMN guild_storage_id character varying(12) NOT NULL DEFAULT 'foo';");
@@ -58,7 +35,7 @@ const tasks = [
         execute: () => {
             return pool.query(`
                 UPDATE guilds
-                SET guild_storage_id = random_string(12);
+                SET guild_storage_id = 'lJ5kRUem8rEm';
             `);
         }
     },
@@ -120,10 +97,8 @@ const tasks = [
         name: "Fill the storage_id columns for members table",
         execute: () => {
             return pool.query(`
-                UPDATE members AS me
-                SET storage_id = gs.guild_storage_id
-                FROM guilds AS gs
-                WHERE gs.guild_id = me.guild_id;
+                UPDATE members
+                SET storage_id = 'lJ5kRUem8rEm';
             `);
         }
     },
@@ -139,10 +114,8 @@ const tasks = [
         name: "Fill the storage_id column in invited_members table",
         execute: () => {
             return pool.query(`
-                UPDATE invited_member_events AS me
-                SET storage_id = gs.guild_storage_id
-                FROM guilds AS gs
-                WHERE gs.guild_id = me.guild_id;
+                UPDATE invited_member_events
+                SET storage_id = 'lJ5kRUem8rEm';
             `);
         }
     },
