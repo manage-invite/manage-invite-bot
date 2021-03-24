6d69b90c8b81b7d4d0fca29321b963c148520287
Androz2091 <androz2091@gmail.com>
2021-03-24 18:50:25 +0100

    :zap: Improve migration script

--- scripts/migration-47-48.js
+++ scripts/migration-47-48.js
@@ -166,6 +166,158 @@ const tasks = [
                 ALTER TABLE members ALTER COLUMN invites_bonus TYPE INT;
             `);
         }
+    },
+    {
+        name: "Delete useless guild plugins",
+        execute: () => {
+            return pool.query(`
+                DELETE FROM guild_plugins
+
+                WHERE plugin_data = '{
+                    "enabled": false,
+                    "message": null
+                }'
+                OR plugin_data = '{
+                    "channel": null,
+                    "enabled": false,
+                    "message": null
+                }';
+
+                DELETE FROM guild_plugins T1
+                    USING   guild_plugins T2
+                WHERE   T1.ctid < T2.ctid 
+                    AND T1.guild_id = T2.guild_id 
+                    AND T1.plugin_name  = T2.plugin_name;
+            `);
+        }
+    },
+    {
+        name: "Create guild plugins unique constraint",
+        execute: () => {
+            return pool.query(`
+                ALTER TABLE guild_plugins
+                ADD UNIQUE (guild_id, plugin_name);
+            `);
+        }
+    },
+    {
+        name: "Delete useless guild blacklisted users",
+        execute: () => {
+            return pool.query(`
+                DELETE FROM guild_blacklisted_users T1
+                    USING   guild_blacklisted_users T2
+                WHERE   T1.ctid < T2.ctid 
+                    AND T1.guild_id = T2.guild_id 
+                    AND T1.user_id  = T2.user_id;
+            `);
+        }
+    },
+    {
+        name: "Create guild blacklisted users unique constraint",
+        execute: () => {
+            return pool.query(`
+                ALTER TABLE guild_blacklisted_users
+                ADD UNIQUE (guild_id, user_id);
+            `);
+        }
+    },
+    {
+        name: "Create guild ranks unique constraint",
+        execute: () => {
+            return pool.query(`
+                ALTER TABLE guild_ranks
+                ADD UNIQUE (guild_id, role_id);
+            `);
+        }
+    },
+    {
+        name: "Delete useless guild subscriptions",
+        execute: () => {
+            return pool.query(`
+                DELETE FROM guild_subscriptions T1
+                    USING   guild_subscriptions T2
+                WHERE   T1.ctid < T2.ctid 
+                    AND T1.guild_id = T2.guild_id 
+                    AND T1.sub_id  = T2.sub_id;
+            `);
+        }
+    },
+    {
+        name: "Create guild subscriptions unique constraint",
+        execute: () => {
+            return pool.query(`
+                ALTER TABLE guild_subscriptions
+                ADD UNIQUE (guild_id, sub_id);
+            `);
+        }
+    },
+    {
+        name: "Delete useless subscriptions payments",
+        execute: () => {
+            return pool.query(`
+                DELETE FROM subscriptions_payments T1
+                    USING   subscriptions_payments T2
+                WHERE   T1.ctid < T2.ctid 
+                    AND T1.payment_id = T2.payment_id 
+                    AND T1.sub_id  = T2.sub_id;
+            `);
+        }
+    },
+    {
+        name: "Create subscriptions payments unique constraint",
+        execute: () => {
+            return pool.query(`
+                ALTER TABLE guild_subscriptions
+                ADD UNIQUE (payment_id, sub_id);
+            `);
+        }
+    },
+    {
+        name: "Create guild storages index",
+        execute: () => {
+            return pool.query(`
+                CREATE INDEX idx_guild_storages_guild_id
+                ON guild_storages (guild_id);
+            `);
+        }
+    },
+    // QUELS INDEX CREER POUR OPTIMISER LA RECUPERATION DES ABONNEMENTS ?
+    // AJOUTER INDEX SUR subscriptions_payments ET guilds_subscriptions => TABLE SEULEMENT UTILISÉE POUR DES JOINTURES
+    {
+        name: "Create members index",
+        execute: () => {
+            return pool.query(`
+                CREATE INDEX idx_members_user_id_guild_id
+                ON members (user_id, guild_id);
+            `);
+        }
+    },
+    {
+        name: "Create members index only guild",
+        execute: () => {
+            return pool.query(`
+                CREATE INDEX idx_members_guild_id
+                ON members (guild_id);
+            `);
+        }
+    },
+    {
+        name: "Create payments index",
+        execute: () => {
+            return pool.query(`
+                CREATE INDEX idx_payments_id
+                ON payments (id);
+            `);
+        }
+    },
+    {
+        name: "Create subscriptions index",
+        execute: () => {
+            return pool.query(`
+                CREATE INDEX idx_subscriptions_id
+                ON subscriptions (id);
+            `);
+        }
     }
 ];
 
