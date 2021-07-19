3f3c10383d650745a0a3eba87545cff4d38ccc9b
Androz2091 <androz2091@gmail.com>
2021-07-19 12:22:03 +0200

    :sparkles: Add setlang slash command

--- commands/config/setlang.js
+++ commands/config/setlang.js
@@ -1,4 +1,5 @@
 const Command = require("../../structures/Command.js");
+const emojis = require("../../emojis.json");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -7,7 +8,22 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "setlanguage", "configlanguage" ],
             clientPermissions: [ "EMBED_LINKS" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Change the language of the guild",
+                options: [
+                    {
+                        name: "language",
+                        type: 3,
+                        description: "The new guild language",
+                        choices: client.enabledLanguages.map((language) => ({
+                            name: `${language.aliases[0]} ${emojis.find((e) => e.name === language.flag).unicode}`,
+                            value: language.name
+                        }))
+                    }
+                ]
+            }
         });
     }
 
@@ -22,5 +38,12 @@ module.exports = class extends Command {
         await this.client.database.updateGuildSetting(message.guild.id, "language", newLanguage);
         message.guild.settings.language = newLanguage;
         message.success("config/setlang:SUCCESS");
+    }
+
+    async runInteraction (interaction) {
+        const newLanguage = interaction.options.getString("language");
+        await this.client.database.updateGuildSetting(interaction.guild.id, "language", newLanguage);
+        interaction.guild.settings.language = newLanguage;
+        interaction.reply(interaction.guild.translate("config/setlang:SUCCESS"));
     }
 };
