4981986cfb299a489dd92820bf0592228d984093
Androz2091 <androz2091@gmail.com>
2021-07-19 10:16:37 +0200

    :bug: Fix member count command

--- commands/core/membercount.js
+++ commands/core/membercount.js
@@ -1,6 +1,5 @@
 const Command = require("../../structures/Command.js"),
-    Discord = require("discord.js"),
-    Constants = require("../../helpers/constants");
+    Discord = require("discord.js");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -25,23 +24,7 @@ module.exports = class extends Command {
                     totalCount: message.guild.members.cache.size,
                     humanCount: message.guild.members.cache.filter((m) => !m.user.bot).size,
                     botCount: message.guild.members.cache.filter((m) => m.user.bot).size
-                }) + "\n" +
-            message.translate("core/membercount:DND", {
-                emoji: Constants.Emojis.DND,
-                count: message.guild.members.cache.filter((m) => m.presence.status === "dnd"  && !m.user.bot).size
-            }) + "\n" +
-            message.translate("core/membercount:ONLINE", {
-                emoji: Constants.Emojis.ONLINE,
-                count: message.guild.members.cache.filter((m) => m.presence.status === "online"  && !m.user.bot).size
-            }) + "\n" +
-            message.translate("core/membercount:IDLE", {
-                emoji: Constants.Emojis.IDLE,
-                count: message.guild.members.cache.filter((m) => m.presence.status === "idle"  && !m.user.bot).size
-            }) + "\n" +
-            message.translate("core/membercount:OFFLINE", {
-                emoji: Constants.Emojis.OFFLINE,
-                count: message.guild.members.cache.filter((m) => m.presence.status === "offline"  && !m.user.bot).size
-            })
+                })
             )
             .setColor(data.color)
             .setFooter(data.footer);
