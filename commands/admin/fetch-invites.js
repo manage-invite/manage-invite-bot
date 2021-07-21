2edd7fc4abd950fb24b94694008537caf71dc60b
Androz2091 <androz2091@gmail.com>
2021-07-21 14:30:06 +0200

    :sparkles: Add fetch invites command

--- commands/admin/fetch-invites.js
+++ commands/admin/fetch-invites.js
@@ -1,5 +1,6 @@
 const Command = require("../../structures/Command.js");
 const { generateInvitesCache } = require("../../helpers/functions.js");
+const Constants = require("../../helpers/constants.js");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -8,7 +9,11 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [],
             clientPermissions: [ "EMBED_LINKS", "MANAGE_GUILD" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Fetch all invites from the current guild."
+            }
         });
     }
 
@@ -16,6 +21,12 @@ module.exports = class extends Command {
         await message.guild.invites.fetch();
         this.client.invitations[message.guild.id] = generateInvitesCache(message.guild.invites.cache);
         message.success("admin/fetch-invites:SUCCESS");
+    }
+
+    async runInteraction (interaction) {
+        await interaction.guild.invites.fetch();
+        this.client.invitations[interaction.guild.id] = generateInvitesCache(interaction.guild.invites.cache);
+        interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("admin/fetch-invites:SUCCESS") });
     }
 
 };
