382e3a4461e367a0c32295570082f42be57a1ccc
Androz2091 <androz2091@gmail.com>
2021-07-21 12:00:22 +0200

    :sparkles: Add removeinvites slash command

--- commands/admin/removeinvites.js
+++ commands/admin/removeinvites.js
@@ -9,39 +9,130 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "rinvites", "removeinv", "rinv", "removeinvite", "remove-invites", "remove-invite" ],
             clientPermissions: [ "EMBED_LINKS" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Remove all the server invites"
+            }
         });
     }
 
     async run (message, args, data) {
         
-        const conf = await message.sendT("admin/removeinvites:CONFIRMATION", {
-            error: Constants.Emojis.ERROR,
-            success: Constants.Emojis.SUCCESS
+        const confirmRow = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setStyle("SUCCESS")
+                    .setLabel(message.translate("common:CONFIRM"))
+                    .setCustomId("confirm"),
+                new Discord.MessageButton()
+                    .setStyle("SECONDARY")
+                    .setLabel(message.translate("common:CANCEL"))
+                    .setCustomId("cancel")
+            );
+
+        const conf = await message.channel.send({ content: message.translate("admin/removeinvites:CONFIRMATION"), components: [confirmRow] });
+        const collector = conf.createMessageComponentCollector({
+            filter: () => true,
+            time: 90000
         });
-        await message.channel.awaitMessages({ filter: (m) => m.author.id === message.author.id && (m.content === "cancel" || m.content === "-confirm"), max: 1, time: 90000 }).then(async (collected) => {
-            if (collected.first().content === "cancel") return conf.error("common:CANCELLED", null, true);
-            collected.first().delete().catch(() => {});
 
-            await conf.sendT("admin/removeinvites:LOADING", {
-                loading: Constants.Emojis.LOADING,
-                prefix: message.guild.settings.prefix
-            }, true);
+        collector.on("collect", async (component) => {
 
-            await this.client.database.removeGuildInvites(message.guild.id);
+            if (component.customId === "confirm") {
 
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(message.translate("admin/removeinvites:TITLE"))
-                .setDescription(message.translate("admin/removeinvites:DESCRIPTION", {
-                    success: Constants.Emojis.SUCCESS
-                }))
-                .setColor(data.color)
-                .setFooter(data.footer);
+                await conf.edit({ content: message.translate("admin/removeinvites:LOADING", {
+                    loading: Constants.Emojis.LOADING,
+                    prefix: message.guild.settings.prefix
+                }), components: [] });
+        
+                await this.client.database.removeGuildInvites(message.guild.id);
+        
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(message.translate("admin/removeinvites:TITLE"))
+                    .setDescription(message.translate("admin/removeinvites:DESCRIPTION", {
+                        success: Constants.Emojis.SUCCESS
+                    }))
+                    .setColor(data.color)
+                    .setFooter(data.footer);
+        
+                conf.edit({ embeds: [embed] });
 
-            conf.edit({ embeds: [embed] });
-        }).catch((err) => {
-            console.error(err);
-            return conf.error("common:CANCELLED", null, true);
+            } else if (component.customId === "cancel") {
+                conf.edit({ content: Constants.Emojis.SUCCESS + " " + message.translate("common:CANCELLED"), components: [] });
+                collector.stop();
+            }
+
+            component.deferUpdate();
+
+        });
+
+        collector.on("end", (_, reason) => {
+            if (reason === "time") {
+                conf.edit({ content: Constants.Emojis.ERROR + " " + message.translate("common:CANCELLED"), components: [] });
+            }
+        });
+
+    }
+
+    async runInteraction (interaction, data) {
+
+        const randomID = Math.random().toString(36).substr(2, 9);
+        
+        const confirmRow = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setStyle("SUCCESS")
+                    .setLabel(interaction.guild.translate("common:CONFIRM"))
+                    .setCustomId(`confirm_${randomID}`),
+                new Discord.MessageButton()
+                    .setStyle("SECONDARY")
+                    .setLabel(interaction.guild.translate("common:CANCEL"))
+                    .setCustomId(`cancel_${randomID}`)
+            );
+
+        await interaction.reply({ content: interaction.guild.translate("admin/removeinvites:CONFIRMATION"), components: [confirmRow] });
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter: (i) => i.customId.endsWith(randomID),
+            time: 90000
+        });
+
+        collector.on("collect", async (component) => {
+
+            const action = component.customId.split("_")[0];
+
+            if (action === "confirm") {
+
+                await interaction.editReply({ content: interaction.guild.translate("admin/removeinvites:LOADING", {
+                    loading: Constants.Emojis.LOADING,
+                    prefix: interaction.guild.settings.prefix
+                }), components: [] });
+        
+                await this.client.database.removeGuildInvites(interaction.guild.id);
+        
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(interaction.guild.translate("admin/removeinvites:TITLE"))
+                    .setDescription(interaction.guild.translate("admin/removeinvites:DESCRIPTION", {
+                        success: Constants.Emojis.SUCCESS
+                    }))
+                    .setColor(data.color)
+                    .setFooter(data.footer);
+        
+                interaction.editReply({ content: null, embeds: [embed] });
+
+            } else if (action === "cancel") {
+                interaction.editReply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("common:CANCELLED"), components: [] });
+                collector.stop();
+            }
+
+            component.deferUpdate();
+
+        });
+
+        collector.on("end", (_, reason) => {
+            if (reason === "time") {
+                interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("common:CANCELLED"), components: [] });
+            }
         });
 
     }
