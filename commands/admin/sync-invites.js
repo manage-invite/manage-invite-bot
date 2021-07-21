72c79dd9b92ca211795e5d3d8f1d375d59922bad
Androz2091 <androz2091@gmail.com>
2021-07-21 13:57:26 +0200

    :sparkles: Add sync-invites command

--- commands/admin/sync-invites.js
+++ commands/admin/sync-invites.js
@@ -9,7 +9,11 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "sync" ],
             clientPermissions: [ "MANAGE_GUILD" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Sync invites with the ManageInvite database"
+            }
         });
     }
 
@@ -18,44 +22,143 @@ module.exports = class extends Command {
         const guildInvites = message.guild.invites.cache;
         if (guildInvites.size === 0) return message.error("admin/sync-invites:NO_INVITES");
         const inviteCount = guildInvites.map((i) => i.uses).reduce((p, c) => p + c);
-        const conf = await message.sendT("admin/sync-invites:CONFIRM", {
-            success: Constants.Emojis.SUCCESS,
-            error: Constants.Emojis.ERROR,
-            inviteCount
+        const confirmRow = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setStyle("SUCCESS")
+                    .setLabel(message.translate("common:CONFIRM"))
+                    .setCustomId("confirm"),
+                new Discord.MessageButton()
+                    .setStyle("SECONDARY")
+                    .setLabel(message.translate("common:CANCEL"))
+                    .setCustomId("cancel")
+            );
+
+        const conf = await message.channel.send({ content: message.translate("admin/sync-invites:CONFIRM", { inviteCount }), components: [confirmRow] });
+        const collector = conf.createMessageComponentCollector({
+            filter: () => true,
+            time: 90000
         });
-        await message.channel.awaitMessages((m) => m.author.id === message.author.id && (m.content === "cancel" || m.content === "-confirm"), { max: 1, time: 90000 }).then(async (collected) => {
-            if (collected.first().content === "cancel") return conf.error("common:CANCELLED", null, true);
-            collected.first().delete().catch(() => {});
-            const users = new Set(guildInvites.filter((i) => i.inviter).map((i) => i.inviter.id));
-            const newStorageID = await this.client.database.removeGuildInvites(message.guild.id);
-            await this.client.functions.asyncForEach(Array.from(users), async (user) => {
-                const memberData = await this.client.database.fetchGuildMember({
-                    userID: user,
-                    guildID: message.guild.id,
-                    storageID: newStorageID
+
+        collector.on("collect", async (component) => {
+
+            if (component.customId === "confirm") {
+
+                const users = new Set(guildInvites.filter((i) => i.inviter).map((i) => i.inviter.id));
+                const newStorageID = await this.client.database.removeGuildInvites(message.guild.id);
+                await this.client.functions.asyncForEach(Array.from(users), async (user) => {
+                    const memberData = await this.client.database.fetchGuildMember({
+                        userID: user,
+                        guildID: message.guild.id,
+                        storageID: newStorageID
+                    });
+                    if (memberData.notCreated) await this.client.database.createGuildMember({
+                        userID: user,
+                        guildID: message.guild.id,
+                        storageID: newStorageID
+                    });
+                    await this.client.database.addInvites({
+                        userID: user,
+                        guildID: message.guild.id,
+                        storageID: newStorageID,
+                        number: guildInvites.filter((i) => i.inviter && i.inviter.id === user).map((i) => i.uses).reduce((p, c) => p + c),
+                        type: "regular"
+                    });
                 });
-                if (memberData.notCreated) await this.client.database.createGuildMember({
-                    userID: user,
-                    guildID: message.guild.id,
-                    storageID: newStorageID
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(message.translate("admin/sync-invites:TITLE"))
+                    .setDescription(message.translate("admin/sync-invites:DESCRIPTION"))
+                    .setColor(data.color)
+                    .setFooter(data.footer);
+                conf.edit({ content: null, embeds: [embed], components: [] });
+
+            } else if (component.customId === "cancel") {
+                conf.edit({ content: Constants.Emojis.SUCCESS + " " + message.translate("common:CANCELLED"), components: [] });
+            }
+
+            component.deferUpdate();
+
+        });
+
+        collector.on("end", (_, reason) => {
+            if (reason === "time") {
+                conf.edit({ content: Constants.Emojis.ERROR + " " + message.translate("common:CANCELLED"), components: [] });
+            }
+        });
+    }
+
+    async runInteraction (interaction, data) {
+        await interaction.guild.invites.fetch();
+        const guildInvites = interaction.guild.invites.cache;
+        if (guildInvites.size === 0) return interaction.reply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("admin/sync-invites:NO_INVITES") });
+        const inviteCount = guildInvites.map((i) => i.uses).reduce((p, c) => p + c);
+
+        const randomID = Math.random().toString(36).substring(2, 9);
+
+        const confirmRow = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setStyle("SUCCESS")
+                    .setLabel(interaction.guild.translate("common:CONFIRM"))
+                    .setCustomId(`confirm_${randomID}`),
+                new Discord.MessageButton()
+                    .setStyle("SECONDARY")
+                    .setLabel(interaction.guild.translate("common:CANCEL"))
+                    .setCustomId(`cancel_${randomID}`)
+            );
+
+        await interaction.reply({ content: interaction.guild.translate("admin/sync-invites:CONFIRM", { inviteCount }), components: [confirmRow] });
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter: (i) => i.customId.endsWith(randomID),
+            time: 90000
+        });
+
+        collector.on("collect", async (component) => {
+
+            const action = component.customId.split("_")[0];
+
+            if (action === "confirm") {
+
+                const users = new Set(guildInvites.filter((i) => i.inviter).map((i) => i.inviter.id));
+                const newStorageID = await this.client.database.removeGuildInvites(interaction.guild.id);
+                await this.client.functions.asyncForEach(Array.from(users), async (user) => {
+                    const memberData = await this.client.database.fetchGuildMember({
+                        userID: user,
+                        guildID: interaction.guild.id,
+                        storageID: newStorageID
+                    });
+                    if (memberData.notCreated) await this.client.database.createGuildMember({
+                        userID: user,
+                        guildID: interaction.guild.id,
+                        storageID: newStorageID
+                    });
+                    await this.client.database.addInvites({
+                        userID: user,
+                        guildID: interaction.guild.id,
+                        storageID: newStorageID,
+                        number: guildInvites.filter((i) => i.inviter && i.inviter.id === user).map((i) => i.uses).reduce((p, c) => p + c),
+                        type: "regular"
+                    });
                 });
-                await this.client.database.addInvites({
-                    userID: user,
-                    guildID: message.guild.id,
-                    storageID: newStorageID,
-                    number: guildInvites.filter((i) => i.inviter && i.inviter.id === user).map((i) => i.uses).reduce((p, c) => p + c),
-                    type: "regular"
-                });
-            });
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(message.translate("admin/sync-invites:TITLE"))
-                .setDescription(message.translate("admin/sync-invites:DESCRIPTION"))
-                .setColor(data.color)
-                .setFooter(data.footer);
-            conf.edit({ embeds: [embed] });
-        }).catch((err) => {
-            console.error(err);
-            conf.error("common:CANCELLED", null, true);
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor(interaction.guild.translate("admin/sync-invites:TITLE"))
+                    .setDescription(interaction.guild.translate("admin/sync-invites:DESCRIPTION"))
+                    .setColor(data.color)
+                    .setFooter(data.footer);
+                interaction.editReply({ content: null, embeds: [embed], components: [] });
+
+            } else if (action === "cancel") {
+                interaction.editReply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("common:CANCELLED"), components: [] });
+            }
+
+            component.deferUpdate();
+
+        });
+
+        collector.on("end", (_, reason) => {
+            if (reason === "time") {
+                interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("common:CANCELLED"), components: [] });
+            }
         });
     }
 
