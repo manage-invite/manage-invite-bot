77e6a16d2d49a4ee5b56227433392a6bd2a56b19
Androz2091 <androz2091@gmail.com>
2021-07-21 15:44:55 +0200

    :sparkles: Add configleave slash command

--- commands/config/configleave.js
+++ commands/config/configleave.js
@@ -1,6 +1,7 @@
 const Command = require("../../structures/Command.js"),
     Discord = require("discord.js"),
-    variables = require("../../helpers/variables");
+    variables = require("../../helpers/variables"),
+    Constants = require("../../helpers/constants");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -9,7 +10,11 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "leave", "leaveconfig" ],
             clientPermissions: [ "EMBED_LINKS", "ADMINISTRATOR" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Configure leave messages"
+            }
         });
     }
 
@@ -25,7 +30,7 @@ module.exports = class extends Command {
         }) : "";
         const msg = await message.sendT("config/configleave:INSTRUCTIONS_1", {
             string: `${str}`,
-            variables: variables.filter((v) => !v.ignore).map((variable) => `{${variable.name}} | ${message.translate(`config/configjoin:VARIABLE_${variable.name.toUpperCase()}`)}` + (variable.endPart ? "\n" : "")).join("\n")
+            variables: variables.filter((v) => !v.ignore).map((variable) => `{${variable.name}} | ${message.translate(`config/configleave:VARIABLE_${variable.name.toUpperCase()}`)}` + (variable.endPart ? "\n" : "")).join("\n")
         });
 
         let collected = await message.channel.awaitMessages(opt).catch(() => {});
@@ -49,12 +54,12 @@ module.exports = class extends Command {
         }, true);
         collected.first().delete().catch(() => {});
 
-        msg.sendT("config/configjoindm:SUCCESS", null, true);
+        msg.sendT("config/configleave:SUCCESS", null, true);
 
         const embed = new Discord.MessageEmbed()
             .setTitle(message.translate("config/configleave:TITLE"))
             .addField(message.translate("common:MESSAGE"), confMessage)
-            .addField(message.translate("common:CHANNEL"), channel)
+            .addField(message.translate("common:CHANNEL"), channel.toString())
             .addField(message.translate("common:TEST_IT"), message.translate("config/configleave:TEST", {
                 prefix: message.guild.settings.prefix
             }))
@@ -69,5 +74,63 @@ module.exports = class extends Command {
             mainMessage: confMessage,
             channel: channel.id
         });
+    }
+
+    async runInteraction (interaction, data) {
+
+        const guildPlugins = await this.client.database.fetchGuildPlugins(interaction.guild.id);
+        const plugin = guildPlugins.find((p) => p.pluginName === "leave")?.pluginData;
+
+        const opt = { filter: (m) => m.author.id === interaction.user.id, max: 1, time: 90000, errors: [ "time" ] };
+        
+        const str = plugin?.enabled ? interaction.guild.translate("config/configleave:DISABLE", {
+            prefix: interaction.guild.settings.prefix
+        }) : "";
+        await interaction.reply({ content: interaction.guild.translate("config/configleave:INSTRUCTIONS_1", {
+            string: `${str}`,
+            variables: variables.filter((v) => !v.ignore).map((variable) => `{${variable.name}} | ${interaction.guild.translate(`config/configleave:VARIABLE_${variable.name.toUpperCase()}`)}` + (variable.endPart ? "\n" : "")).join("\n")
+        }) });
+
+        let collected = await interaction.channel.awaitMessages(opt).catch(() => {});
+        if (!collected || !collected.first()) return interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("common:CANCELLED") });
+        const confMessage = collected.first().content;
+        if (confMessage === "cancel") return interaction.editReply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("common:CANCELLED") });
+        if (confMessage === interaction.guild.settings.prefix+"setleave") return;
+        collected.first().delete().catch(() => {});
+
+        interaction.editReply({ content: interaction.guild.translate("config/configleave:INSTRUCTIONS_2") });
+
+        collected = await interaction.channel.awaitMessages(opt).catch(() => {});
+        if (!collected || !collected.first()) return interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("common:CANCELLED") });
+        const confChannel = collected.first();
+        if (confChannel.content === "cancel") return interaction.editReply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("common:CANCELLED") });
+        const channel = confChannel.mentions.channels.first()
+        || interaction.guild.channels.cache.get(confChannel.content)
+        || interaction.guild.channels.cache.find((ch) => ch.name === confChannel.content || `#${ch.name}` === confChannel.content);
+        if (!channel || channel.type === "voice") return interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("config/configleave:CHANNEL_NOT_FOUND", {
+            channel: confChannel.content
+        }) });
+        collected.first().delete().catch(() => {});
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(interaction.guild.translate("config/configleaven:TITLE"))
+            .addField(interaction.guild.translate("common:MESSAGE"), confMessage)
+            .addField(interaction.guild.translate("common:CHANNEL"), channel.toString())
+            .addField(interaction.guild.translate("common:TEST_IT"), interaction.guild.translate("config/configleave:TEST", {
+                prefix: interaction.guild.settings.prefix
+            }))
+            .setThumbnail(interaction.user.avatarURL())
+            .setColor(data.color)
+            .setFooter(data.footer);
+
+        interaction.editReply({ content: interaction.guild.translate("config/configleavedm:SUCCESS"), embeds: [embed] });
+
+        await this.client.database.updateGuildPlugin(interaction.guild.id, "leave", {
+            ...(plugin || {}),
+            enabled: true,
+            mainMessage: confMessage,
+            channel: channel.id
+        });
+
     }
 };
