f33382e68276d716dc9540286abc33c8c03eebd4
Androz2091 <androz2091@gmail.com>
2021-07-21 17:14:02 +0200

    :sparkles: add setjoin slash command

--- commands/config/setjoin.js
+++ commands/config/setjoin.js
@@ -1,4 +1,5 @@
 const Command = require("../../structures/Command.js");
+const Constants = require("../../helpers/constants");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -7,7 +8,11 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [],
             clientPermissions: [ "EMBED_LINKS" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Enable or disable the join message"
+            }
         });
     }
 
@@ -28,6 +33,26 @@ module.exports = class extends Command {
                 enabled: false
             });
             return message.success("config/setjoin:DISABLED");
+        }
+    }
+
+    async runInteraction (interaction) {
+        const guildPlugins = await this.client.database.fetchGuildPlugins(interaction.guild.id);
+        const plugin = guildPlugins.find((p) => p.pluginName === "join")?.pluginData;
+
+        if (!plugin?.enabled){
+            await this.client.database.updateGuildPlugin(interaction.guild.id, "join", {
+                ...(plugin || {}),
+                enabled: true
+            });
+            return interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("config/setjoin:ENABLED") });
+        }
+        if (plugin.enabled){
+            await this.client.database.updateGuildPlugin(interaction.guild.id, "join", {
+                ...plugin,
+                enabled: false
+            });
+            return interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("config/setjoin:DISABLED") });
         }
     }
 };
