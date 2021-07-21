14ff4d2fb5e59114af028006ec25c734c3467cdc
Androz2091 <androz2091@gmail.com>
2021-07-21 14:41:17 +0200

    :sparkles: Add cmd-channel slash command

--- commands/config/cmd-channel.js
+++ commands/config/cmd-channel.js
@@ -1,4 +1,5 @@
 const Command = require("../../structures/Command.js");
+const Constants = require("../../helpers/constants");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -7,7 +8,31 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "cmdchannel" ],
             clientPermissions: [ "EMBED_LINKS", "MANAGE_MESSAGES" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Enable or disable the command channel.",
+                options: [
+                    {
+                        name: "set",
+                        description: "Enable or change the command channel",
+                        type: 1,
+                        options: [
+                            {
+                                name: "channel",
+                                description: "The channel to set the command channel to.",
+                                type: 7,
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        name: "disable",
+                        description: "Disable the command channel.",
+                        type: 1
+                    }
+                ]
+            }
         });
     }
 
@@ -24,6 +49,27 @@ module.exports = class extends Command {
                 message.success("config/cmd-channel:SUCCESS_DISABLED");
             } else {
                 message.error("config/cmd-channel:MISSING_CHANNEL");
+            }
+        }
+    }
+
+    async runInteraction (interaction) {
+
+        const action = interaction.options.getSubCommand();
+
+        if (action === "set") {
+            const channel = interaction.options.getChannel("channel");
+            await this.client.database.updateGuildSetting(interaction.guild.id, "cmdChannel", channel.id);
+            interaction.reply({ content: interaction.guild.translate("config/cmd-channel:SUCCESS_ENABLED", {
+                channel: channel.toString(),
+                prefix: interaction.guild.settings.prefix
+            }) });
+        } else if (action === "disable") {
+            if (interaction.guild.settings.cmdChannel){
+                await this.client.database.updateGuildSetting(interaction.guild.id, "cmdChannel", null);
+                interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("config/cmd-channel:SUCCESS_DISABLED") });
+            } else {
+                interaction.reply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("config/cmd-channel:DISABLE_NO_CHANNEL") });
             }
         }
     }
