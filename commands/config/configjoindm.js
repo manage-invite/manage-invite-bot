f4829d3ee4d37c49a0dc1bc2e25dda8162f34961
Androz2091 <androz2091@gmail.com>
2021-07-21 15:44:51 +0200

    :sparkles: Add configjoindm slash command

--- commands/config/configjoindm.js
+++ commands/config/configjoindm.js
@@ -1,6 +1,7 @@
 const Command = require("../../structures/Command.js"),
     Discord = require("discord.js"),
-    variables = require("../../helpers/variables");
+    variables = require("../../helpers/variables"),
+    Constants = require("../../helpers/constants");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -9,7 +10,11 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [ "dmjoin", "joindm", "configjoindm", "dm", "configdm" ],
             clientPermissions: [ "EMBED_LINKS", "ADMINISTRATOR" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Configure join DM messages"
+            }
         });
     }
 
@@ -28,7 +33,7 @@ module.exports = class extends Command {
             variables: variables.filter((v) => !v.ignore).map((variable) => `{${variable.name}} | ${message.translate(`config/configjoin:VARIABLE_${variable.name.toUpperCase()}`)}` + (variable.endPart ? "\n" : "")).join("\n")
         });
 
-        const collected = await message.channel.awaitMessages(filter, opt).catch(() => {});
+        const collected = await message.channel.awaitMessages(opt).catch(() => {});
         if (!collected || !collected.first()) return msg.error("common:CANCELLED", null, true);
         const confMessage = collected.first().content;
         if (confMessage === "cancel") return msg.error("common:CANCELLED", null, true);
@@ -52,6 +57,47 @@ module.exports = class extends Command {
             enabled: true,
             mainMessage: confMessage
         });
+    }
+
+    async runInteraction (interaction, data) {
+
+        const guildPlugins = await this.client.database.fetchGuildPlugins(interaction.guild.id);
+        const plugin = guildPlugins.find((p) => p.pluginName === "joinDM")?.pluginData;
+
+        const opt = { filter: (m) => m.author.id === interaction.user.id, max: 1, time: 90000, errors: [ "time" ] };
+        
+        const str = plugin?.enabled ? interaction.translate("config/configjoindm:DISABLE", {
+            prefix: interaction.guild.settings.prefix
+        }) : "";
+        await interaction.reply({ content: interaction.guild.translate("config/configjoindm:INSTRUCTIONS_1", {
+            string: `${str}`,
+            variables: variables.filter((v) => !v.ignore).map((variable) => `{${variable.name}} | ${interaction.guild.translate(`config/configjoin:VARIABLE_${variable.name.toUpperCase()}`)}` + (variable.endPart ? "\n" : "")).join("\n")
+        }) });
+
+        const collected = await interaction.channel.awaitMessages(opt).catch(() => {});
+        if (!collected || !collected.first()) return interaction.editReply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("common:CANCELLED") });
+        const confMessage = collected.first().content;
+        if (confMessage === "cancel") return interaction.editReply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("common:CANCELLED") });
+        if (confMessage === interaction.guild.settings.prefix+"setdmjoin") return;
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(interaction.guild.translate("config/configjoindm:TITLE"))
+            .addField(interaction.guild.translate("common:MESSAGE"), confMessage)
+            .addField(interaction.guild.translate("common:TEST_IT"), interaction.guild.translate("config/configjoindm:TEST", {
+                prefix: interaction.guild.settings.prefix
+            }))
+            .setThumbnail(interaction.user.avatarURL())
+            .setColor(data.color)
+            .setFooter(data.footer);
+
+        interaction.editReply({ content: interaction.guild.translate("config/configjoindm:SUCCESS"), embeds: [embed] });
+
+        await this.client.database.updateGuildPlugin(interaction.guild.id, "joinDM", {
+            ...(plugin || {}),
+            enabled: true,
+            mainMessage: confMessage
+        });
+
     }
 
 };
