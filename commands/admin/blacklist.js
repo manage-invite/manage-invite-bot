b5ef7be63b59e40e34de2300db207d5668136c7f
Androz2091 <androz2091@gmail.com>
2021-07-21 14:30:21 +0200

    :sparkles: Add blacklist slash command

--- commands/admin/blacklist.js
+++ commands/admin/blacklist.js
@@ -1,5 +1,6 @@
 const Command = require("../../structures/Command.js"),
-    Discord = require("discord.js");
+    Discord = require("discord.js"),
+    Constants = require("../../helpers/constants");
 
 module.exports = class extends Command {
     constructor (client) {
@@ -8,7 +9,44 @@ module.exports = class extends Command {
             enabled: true,
             aliases: [],
             clientPermissions: [ "EMBED_LINKS" ],
-            permLevel: 2
+            permLevel: 2,
+
+            slashCommandOptions: {
+                description: "Blacklist a user from using the bot and being shown on the leaderboard",
+                options: [
+                    {
+                        name: "add",
+                        type: 1,
+                        description: "Add a user to the blacklist",
+                        options: [
+                            {
+                                name: "user",
+                                description: "The user to blacklist",
+                                type: 6,
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        name: "remove",
+                        type: 1,
+                        description: "Remove a user from the blacklist",
+                        options: [
+                            {
+                                name: "user",
+                                description: "The user to remove from the blacklist",
+                                type: 6,
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        name: "list",
+                        type: 1,
+                        description: "List all the blacklisted users"
+                    }
+                ]
+            }
         });
     }
 
@@ -69,6 +107,64 @@ module.exports = class extends Command {
             message.error("admin/blacklist:MISSING_TYPE");
         }
         }
+    }
+
+    async runInteraction (interaction, data) {
+
+        const guildBlacklistedUsers = await this.client.database.fetchGuildBlacklistedUsers(interaction.guild.id);
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(data.color)
+            .setFooter(data.footer);
+        const action = interaction.options.getSubCommand();
+        switch (action){
+        case "add": {
+            const user = interaction.options.getUser("user");
+            if (guildBlacklistedUsers.includes(user.id)) return interaction.reply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("admin/blacklist:ALREADY_BLACKLISTED", {
+                username: user.tag
+            }) });
+            await this.client.database.addGuildBlacklistedUser({
+                guildID: interaction.guild.id,
+                userID: user.id
+            });
+            interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("admin/blacklist:SUCCESS_MEMBER_ADD", {
+                username: user.tag
+            }) });
+            break;
+        }
+        case "remove": {
+            const user = interaction.options.getUser("user");
+            if (!guildBlacklistedUsers.includes(user.id)) return interaction.reply({ content: Constants.Emojis.ERROR + " " + interaction.guild.translate("admin/blacklist:NOT_BLACKLISTED", {
+                username: user.tag
+            }) });
+            await this.client.database.removeGuildBlacklistedUser({
+                guildID: interaction.guild.id,
+                userID: user.id
+            });
+            interaction.reply({ content: Constants.Emojis.SUCCESS + " " + interaction.guild.translate("admin/blacklist:SUCCESS_MEMBER_REMOVE", {
+                username: user.tag
+            }) });
+            break;
+        }
+        case "list": {
+            if (guildBlacklistedUsers.length < 1){
+                embed.setDescription(interaction.guild.translate("admin/blacklist:EMPTY"));
+            } else {
+                const users = [];
+                await this.client.functions.asyncForEach(guildBlacklistedUsers, async (userID) => {
+                    const user = await this.client.users.fetch(userID);
+                    users.push(`${user.tag} (${user.toString()})`);
+                });
+                embed.setDescription(users.join("\n"));
+            }
+            interaction.reply({ embeds: [embed] });
+            break;
+        }
+        default: {
+            interaction.reply({ content: "admin/blacklist:MISSING_TYPE" });
+        }
+        }
+
     }
 
 };
